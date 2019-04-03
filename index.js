b6bf2e769705add287be855d8321aa7386b0d2bd
Justin Grayston <jgrayston@google.com>
2019-04-03 23:23:00 +0100

    Add docs

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const message = process.env['MESSAGE'] || 'No message set';
 
+/**
+ * A simple function that response with a message set by environment
+ * variable.
+ * @param {Request} req
+ * @param {Response} res
+ */
 exports.helloWorld = (req, res) => {
   res.send(message);
 }
